abf5eab95c9a0dbe7179021f3e4bcb7da66039e7
Eugene <viiper94@gmail.com>
2016-12-19 17:23:01 +0200

    fixed navbar position on page load

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,13 @@ $(document).ready(function(){
 
     // making navbar floating
     var navbarPos = $('.navbar').offset();
+    scrollNavbar();
+
     window.onscroll = function(){
+        scrollNavbar();
+    }
+
+    function scrollNavbar(){
         if(window.pageYOffset >= navbarPos.top){
             $('.navbar').removeClass('bottom');
             $('.navbar').addClass('sticked');
