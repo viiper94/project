16ad6e66265221ebe60ef305cecc50555696c087
Eugene <viiper94@gmail.com>
2016-12-23 17:42:54 +0200

    added more ajax to navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -74,43 +74,96 @@ $(document).ready(function(){
     
     $(document).on('click', '.ajaxable', function(){
         event.preventDefault();
-        var target = $(this).attr('data-target');
-        var page = $(this).attr('data-page');
-        var id = $(this).attr('data-id');
-        var section = id == undefined ? target+'-list' : target;
-        //console.log(this);
+        var parameters = {
+            'target' : $(this).attr('data-target'),                                                         // get target controller
+            'page' : $(this).attr('data-page'),                                                             // get page
+            'id' : $(this).attr('data-id')                                                                  // get id
+        };
+        parameters.section = parameters.id == undefined ? parameters.target+'-list' : parameters.target;    // generate html template name
+        // if clicked href has hash
+        if($(this).attr('href').indexOf('#') != -1){
+            var href = $(this).attr('href');
+            parameters.hash = href.substring(href.indexOf('#'));                                            // get anchor in school page
+            // console.log(hash);
+        }
+        ajaxRequest(parameters);
+    });
+
+    $('#search-form').submit(function(){
+        event.preventDefault();
+        var data = {
+            'target' : 'search',
+            'section' : 'search',
+            'query' : $('#search-form input').val()
+        };
+        // console.log(query);
+        ajaxRequest(data);
+    });
+
+    function ajaxRequest(data){
         $.ajax({
             cache: false,
             dataType : 'json',
             type : 'GET',
-            url : '/'+target,
+            url : '/'+data.target,
             data : {
                 'ajax': 'true',
-                'page' : page,
-                'id' : id
+                'page' : data.page,
+                'id' : data.id,
+                'q' : data.query
             },
             success : function(response){
                 if(response.status == 'OK'){
                     clearMain();
-                    render(section, response);
-                    if(response.pgn != undefined) pagination(target, response.pgn);
+                    setNewTitle(response.title);
+                    if(data.section === 'search') renderSearch(response);
+                    else renderTemplate(data.section, response);
+                    if(response.pgn != undefined) pagination(data.target, response.pgn);
+                    if(data.hash != undefined){
+                        $('html, body').animate({
+                            scrollTop: $(data.hash).offset().top
+                        }, 800);
+                    }
                 }else{
                     alert(response.status);
                 }
             }
-        })
-    });
+        });
+    }
 
     function clearMain(){
         $('main').html('');
     }
+    
+    function setNewTitle(title){
+        $('title').html(title);
+    }
 
-    function render(tmpl, data){
+    function renderTemplate(tmpl, data){
         var template = _.template($('#'+tmpl+'-template').html())(data);
-        //console.log(data);
+        console.log(data);
         $('main').append(template);
     }
-    
+
+    function renderSearch(data){
+        var item = {'releases' : []};
+        $.each(data.releases.title, function(index, value){
+            item.releases[index] = value;
+        });
+        $.each(data.releases.artist, function(index, value){
+            item.releases[index] = value;
+        });
+        console.log(item);
+        var templates = {};
+        templates.newsTemplate = data.news != false ? _.template($('#news-list-template').html())(data) : undefined;
+        templates.releasesTemplate = $(item.artists).length > 0 || $(item.title).length > 0
+            ? _.template($('#releases-list-template').html())(item)
+            : undefined;
+        templates.artistsTemplate = data.artists != false ? _.template($('#artists-list-template').html())(data) : undefined;
+        var searchTemplate = _.template($('#search-template').html())(templates);
+        $('main').append(searchTemplate);
+    }
+
     function pagination(controller, pages){
         //console.log(pages);
         var data = {
